f9034a60945c771bdbee6dbde98892b82328dd66
fimdomeio <alexandre@fimdomeio.com>
2015-03-16 10:55:29 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+
+Start:
+
+```sh
+npm i
+```
+
+Live build and reload:
+
+```sh
+npm run dev
+```
+
+With extra verbose log:
+
+```sh
+DEBUG=* npm run dev
+```
+
+----
+
+http://leaverou.github.io/awesomplete/ autocomplete without css
+
+why http://lea.verou.me/2015/02/awesomplete-2kb-autocomplete-with-zero-dependencies/
---
+++ build.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var Metalsmith = require('metalsmith'),
+  markdown = require('metalsmith-markdown'),
+  templates = require('metalsmith-templates'),
+  collections = require('metalsmith-collections'),
+  permalinks = require('metalsmith-permalinks'),
+  debug = require('debug');
+
+var log = debug('build');
+var metalsmith = new Metalsmith(__dirname);
+
+metalsmith
+// .use(setTemplate({
+//   pattern: 'projects#<{(||)}>#*.md',
+//   template: 'projects.html'
+// }))
+  .use(markdown())
+  .use(collections({
+    articles: {
+      pattern: 'articles/**/*.md',
+      sortBy: 'date',
+      reverse: true,
+      limit: 10
+    }
+  }))
+  .use(permalinks({
+    pattern: ':title'
+  }))
+  .use(templates({
+    engine: 'handlebars',
+    directory: 'templates'
+  }))
+  // .use(deleteDrafts())
+  .destination('./build')
+  .build(function(err) {
+    // var m;
+    if (err) log('Error! %s', err);
+
+    // m = metalsmith.metadata();
+    // log('What? ', m);
+  });
+
+
+// we can create any metadata we need! (draft is just one example)
+function deleteDrafts() {
+  return function drafts(files, ms, done) {
+    Object.keys(files).forEach(function(file) {
+      log('delete draft %s', file);
+      if (files[file].draft) delete files[file];
+    });
+    done();
+  };
+}
