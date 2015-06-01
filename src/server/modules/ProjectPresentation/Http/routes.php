ddd32bbf8b9faaa75d7b7f90ef0aa01c806f4125
fimdomeio <alexandre@fimdomeio.com>
2015-06-01 16:47:41 +0100

    setting language routes for presentation image

--- src/server/modules/ProjectPresentation/Http/routes.php
+++ src/server/modules/ProjectPresentation/Http/routes.php
@@ -1,6 +1,21 @@
 <?php
 
-Route::group(['namespace' => 'Modules\ProjectPresentation\Http\Controllers'], function()
-{
-	Route::get('/', 'ProjectPresentationController@index');
+$languages = array('en','pt');
+	$locale = Request::segment(1);
+if(in_array($locale, $languages)){
+	\App::setLocale($locale);
+	\View::share('lang', [$locale => true]);
+}else{
+	$locale = null;
+	\View::share('lang', ['en' => true]);
+
+}
+
+Route::group(array('prefix' => $locale), function(){
+
+	Route::group(['namespace' => 'Modules\ProjectPresentation\Http\Controllers'], function()
+	{
+		Route::get('/', 'ProjectPresentationController@index');
+	});
+
 });
