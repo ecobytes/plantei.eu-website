67ce846410a5e09719b92d2c74aa65d41572c398
fimdomeio <alexandre@fimdomeio.com>
2015-06-08 17:52:48 +0100

    Updating project presentation

--- src/server/modules/ProjectPresentation/Http/routes.php
+++ src/server/modules/ProjectPresentation/Http/routes.php
@@ -1,13 +1,14 @@
 <?php
-
-$languages = array('en','pt');
+$languages = Config::get('app.availableLanguages');
 	$locale = Request::segment(1);
 if(in_array($locale, $languages)){
 	\App::setLocale($locale);
 	\View::share('lang', [$locale => true]);
+	\View::share('langString', $locale);
 }else{
 	$locale = null;
 	\View::share('lang', ['en' => true]);
+	\View::share('langString', 'en');
 
 }
 
