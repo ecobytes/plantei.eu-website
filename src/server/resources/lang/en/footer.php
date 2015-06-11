680ec00f72f0922d722fe5bc337b21e650762051
fimdomeio <alexandre@fimdomeio.com>
2015-06-11 17:46:33 +0100

    fixes #4 Footer not being translated

---
+++ src/server/resources/lang/en/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+	'text' => 'plantei.eu <a target="_blank" href="https://github.com/ecobytes/plantei.eu-website">source code</a> is developed under a <a href="https://raw.githubusercontent.com/ecobytes/plantei.eu-website/master/LICENSE" target="_blank">GPL</a> License.'
+
+];
---
+++ src/server/resources/lang/pt/footer.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+	'text' => 'O plantei.eu tem o seu <a target="_blank" href="https://github.com/ecobytes/plantei.eu-website">código</a> desenvolvido sob uma licença open source - <a href="https://raw.githubusercontent.com/ecobytes/plantei.eu-website/master/LICENSE" target="_blank">GPL</a>.'
+
+];
