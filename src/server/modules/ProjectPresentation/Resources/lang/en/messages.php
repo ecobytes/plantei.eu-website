2a38cc2e9661b2bcf754379d04847e0eff67d44c
fimdomeio <alexandre@fimdomeio.com>
2015-06-23 16:24:01 +0100

    Updation project description translations.

--- src/server/modules/ProjectPresentation/Resources/lang/en/messages.php
+++ src/server/modules/ProjectPresentation/Resources/lang/en/messages.php
@@ -1,14 +1,14 @@
 <?php 
 return [
-    'intro' => 'Intro Text Not Available in English',
+    'intro' => 'This projects aimas at promoting food sovereignty, by facilitating the exchange of seeds and related knowledge, through a online platform and offline activities.',
     'mailingListSubscriptionTease' => 'For getting updates on the project subscribe our mailing list.',
     'notManyEmails' => 'We promisse infrequent emails',
-    'descriptionTitle1' => 'Missing Translation',
-    'descriptionContent1' => 'Missing Translation',
-    'descriptionTitle2' => 'Missing Translation',
-    'descriptionContent2' => 'Missing Translation',
-    'descriptionTitle3' => 'Missing Translation',
-    'descriptionContent3' => 'Missing Translation',
+    'descriptionTitle1' => 'Personal Seed Banks',
+    'descriptionContent1' => 'The users can enter the varities of seeds that they have, search for seeds of other users, and exchange with them. In this way, plantei.eu contributes for the decentralization of the productive cycle of the see, lowering the costs associated with food production for farmers or anyone that wants to produce her own food, and also to reduce costs for final consumers. ',
+    'descriptionTitle2' => 'Seeds Encyclopedia',
+    'descriptionContent2' => 'Any person can access and co-produce information about seeds, autoctonous varieties, plants and their different uses, contributing for their preservation, keeping and disseminating the knowledge and culture associated with them, and ehnancing the development of innovation by farmers, urban gardeners, researchers, activists, and so on.',
+    'descriptionTitle3' => 'Offline Realm',
+    'descriptionContent3' => 'In order to support the emergence communities of practice and the dynamics of already existing ones, this is to facilitate such offline initiatives, namely creating an agenda for seed swaps, seminars around the topic, and also to promote the exchange of news.',
    	'yourName' => 'Your name',
 	'yourEmail' => 'Your email',
 	'subscribe' => 'Subscribe'
