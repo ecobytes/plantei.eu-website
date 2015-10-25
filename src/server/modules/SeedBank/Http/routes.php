2e5b73cedbfb6949f177182a8d26988ce6786cd9
vasco <aparelhagem@stress.fm>
2015-10-25 15:09:53 +0000

    authentication is now necessary to access seedbank, removed auto complete from search

--- src/server/modules/SeedBank/Http/routes.php
+++ src/server/modules/SeedBank/Http/routes.php
@@ -2,11 +2,13 @@
 
 Route::group(['prefix' => 'seedbank', 'namespace' => 'Modules\SeedBank\Http\Controllers'], function()
 {
-	Route::get('/', 'SeedBankController@index');
-	Route::get('/register/{id?}', 'SeedBankController@getRegister');
-	Route::post('/register/{id?}', 'SeedBankController@postRegister');
-	Route::get('/search', 'SeedBankController@getSearch');
-	Route::post('/search', 'SeedBankController@postSearch');
-	Route::post('/autocomplete', 'SeedBankController@postAutocomplete');
-	Route::get('/preferences', 'SeedBankController@getPreferences');
+	Route::group(['middleware' => 'auth'], function(){
+		Route::get('/', 'SeedBankController@index');
+		Route::get('/register/{id?}', 'SeedBankController@getRegister');
+		Route::post('/register/{id?}', 'SeedBankController@postRegister');
+		Route::get('/search', 'SeedBankController@getSearch');
+		Route::post('/search', 'SeedBankController@postSearch');
+		Route::post('/autocomplete', 'SeedBankController@postAutocomplete');
+		Route::get('/preferences', 'SeedBankController@getPreferences');
+	});
 });
